c3cf39744612339f5f50022149f41f2c2e2b052b
JohnnyRico117 <johnnyrico117@aol.com>
2019-11-11 22:50:25 +0100

    Merge iOS and master

---
+++ lib/ui/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+ThemeData buildTheme() {
+
+  TextTheme _buildTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline: base.headline.copyWith(
+        fontFamily: 'Merriweather',
+        fontSize: 40.0,
+        color: const Color(0xFF2D91EB),
+      ),
+      title: base.title.copyWith(
+        fontFamily: 'Merriweather',
+        fontSize: 15.0,
+        color: const Color(0xFF807A6B),
+      ),
+      caption: base.caption.copyWith(
+        color: const Color(0xFFCCC5AF),
+      ),
+    );
+  }
+
+  final ThemeData base = ThemeData.light();
+
+  return base.copyWith(
+    textTheme: _buildTextTheme(base.textTheme),
+    primaryColor: const Color(0xFF2D91EB),
+    indicatorColor: const Color(0xFF807A6B),
+    scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+    accentColor: const Color(0xFF2D91EB),
+    iconTheme: IconThemeData(
+      color: const Color(0xFFCCC5AF),
+      size: 20.0,
+    ),
+    buttonColor: Colors.white,
+  );
+}
