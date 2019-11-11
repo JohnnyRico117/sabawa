7249663aef1495121339aa322ba8cc5080ed32ea
user149350 <johnnyrico117@aol.com>
2019-11-11 21:10:33 +0000

    iOS Setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
